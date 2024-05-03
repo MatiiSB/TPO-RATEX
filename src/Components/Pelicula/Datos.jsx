cc9ed9972167369b643a5244dfc2b6e2e29bf77b
Matias San Blas <matiassanblas@gmail.com>
2024-05-03 15:51:38 -0300

    03-05

--- src/Components/Pelicula/Datos.jsx
+++ src/Components/Pelicula/Datos.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import { Button } from "@mui/material";
 import "./InfoPelicula.css"
-
+import Rating from "./Rating"
 function Datos(){
     return(
         <div className="datos">
             <div className="info">
                 <div>
-                    <h1>Nombre Pelicula</h1>
-                    <h2>Actores Varios</h2>
+                    <h1 className="infoTitulo">DUNE: PART TWO</h1>
+                    <p>El duque Paul Atreides se une a los Fremen y comienza su entrenamiento para convertirse en Muad'Dib, mientras trata de evitar el terrible futuro que ha previsto: una guerra santa en su nombre, esparciéndose por todo el universo conocido.</p>
+                    <h2><span style={{textDecoration:"underline", textUnderlineOffset:"3px"}}>Dirección:</span> Denis Villeneuve</h2>
                 </div>
                 <div className="botonera">
-                    <Button>Categoria1</Button>
-                    <Button>Categoria2</Button>
-                    <Button>Categoria3</Button>
+                    <Button id="bts_categorias">Acción</Button>
+                    <Button id="bts_categorias">Aventura</Button>
+                    <Button id="bts_categorias">Drama</Button>
                 </div>
             </div>
             <div className="valoracion">
                 <div>
                     <div>
-                        <div>Estrellitas uwu</div>
-                        <div>Guardados</div>
+                        <div><Rating/></div>
                         <div>Compartir</div>
+                        <Button id="bts_categorias">Mi lista +</Button>
                         <div>
-                            <Button>Play</Button>
+                            <Button id="bts_categorias" >Play</Button>
                         </div>
                     </div>
                     </div>
                 <div className="imgContainer">
-                <img src={require("./dune-wall.jpg")} alt="img pelicula"></img>
+                <img className="PeliPortada" src={require("./dune-wall.jpg")} alt="img pelicula"></img>
                 </div>
             </div>
         </div>
